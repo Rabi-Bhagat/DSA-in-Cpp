b56f829bb904d041bd009306daf5be54b461b643
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-15 20:50:46 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your number Boss:";
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"*";
+        cout<<endl;
+    }
+    
+    
+    
+}
