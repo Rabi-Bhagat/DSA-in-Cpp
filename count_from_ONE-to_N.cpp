3d1a8bf3c8edb312da04b3afb5e245bf5468a86c
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-16 21:31:56 +0530

    Add files via upload

---
+++ count_from_ONE-to_N.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your Number Sirji;"<<endl;
+    cin>>n;
+    cout<<"Printing count from one to n:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<i<<endl;
+       
+    }
+    
+}
