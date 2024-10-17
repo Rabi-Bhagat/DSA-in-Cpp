63ea768968508ac282d274604d4abcfd17990bd9
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-17 22:20:40 +0530

    Add files via upload

---
+++ multiple_variable_in_for_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){  
+  int n;
+    cout<<"Enter your number Boss:";
+    cin>>n;
+
+    for (int i = 0,j= 1; i >=0 &&j>=0; i--,j--)
+    {
+        cout<<i<<" "<<j<<" "<<endl;
+    }
+    
+}
