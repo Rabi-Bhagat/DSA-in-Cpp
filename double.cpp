634a1fdcf3a4c791665d495203eed846903831a5
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-18 21:17:30 +0530

    Add files via upload

---
+++ double.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){  
+  int a;
+    cout<<"Enter your number sir jiu:";
+    cin>>a;
+
+    for (int i = 0,j= 1; i >=0 &&j>=0; i--,j--)
+    {
+        cout<<i<<" "<<j<<" "<<endl;
+    }
+    
+}
