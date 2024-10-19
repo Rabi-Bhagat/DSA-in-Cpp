b932e74c46f992fc88db25dbb94650661f563954
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-19 20:03:59 +0530

    Add files via upload

---
+++ for_loop_without_using_condition_in_same_line.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){  
+  int n;
+    cout<<"Enter your number Boss:";
+    cin>>n;
+        int i =0;
+    for (; ;)
+    {
+        if (  i<=n)
+        {
+            cout<<i<<endl;
+            
+
+        }
+        else{
+            break;
+        }
+        i = i+1;
+        
+    }
+    
+
+}
