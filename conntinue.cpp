46b442e28ba054e1ff1c683f4d08f8ab18ba62c0
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-25 23:17:01 +0530

    Add files via upload

---
+++ conntinue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your number: ";
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Hey! buddy"<<endl;
+        cout<<"Are You Fine buddy?"<<endl;
+        continue;
+
+        cout<<"I am a continue "<<endl;
+    }
+    
+}
